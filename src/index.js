77bc56ae85182a1d49adcf6dc0c5124f8cb3a9d7
boaz <djalminya@gmail.com>
2017-08-19 04:15:06 +0300

    test redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { createStore } from "redux"
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const reducer = function (state, action) {
+    var { type , payload } = action;
+    switch(type) {
+      case "INC":
+          return state + payload
+        break;
+      case "DEC":
+          return state - payload;
+        break;
+    }
+    return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("store change", store.getState())
+});
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "DEC", payload: 2});
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
