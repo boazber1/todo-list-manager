44e715d7b5c8444d4298a06448670ff8a30a7aec
boaz <djalminya@gmail.com>
2017-08-19 11:28:06 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { userReducer } from "./reducers/todolistReduserces";
+import { userReducer, todoReducer } from "./reducers/todolistReduserces";
 import { Provider } from "react-redux";
 
 const reducers = combineReducers({
   user: userReducer
+  todoList: todoReducer
 })
 
 const store = createStore(reducers);
@@ -16,11 +17,6 @@ const store = createStore(reducers);
 store.subscribe(() => {
   console.log("store change", store.getState())
 });
-
-store.dispatch({type: "USER_LOGIN_PANDDING", payload: "hi"});
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "INC", payload: 1});
-store.dispatch({type: "DEC", payload: 2});
 
 ReactDOM.render(
         <Provider store={store}>
--- src/reducers/todolistReduserces.js
+++ src/reducers/todolistReduserces.js
@@ -16,3 +16,21 @@ export const userReducer = function (state = {}, action) {
     }
     return state;
 }
+
+export const todoReducer = function (state = {}, action) {
+    var { type } = action;
+    switch (type) {
+      case "TODO_FATCH_PANDING":
+        state = {...state, loading: true}
+        break;
+      case "TODO_FATCH_FULFILLED":
+        var { payload } = action;
+        state = {...state, todos: payload, loading: false};
+        break;
+      case "TODO_FATCH_REJECTED":
+        state = {...state, todos: [], loading: false};
+        breakl
+
+    }
+    return state;
+}
