9a0d66a8d3c6d5c64e8261de7f5a36cb675663ab
git <djalminya@gmail.com>
2017-08-23 16:15:57 +0300

    add react-router and history@1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import { Provider } from "react-redux";
 import { userReducer, todoReducer } from "./reducers/todolistReduserces";
+import logger from "redux-logger"
 import createSagaMiddleware  from "redux-saga"
 import rootSaga from "./networkLayer/api-middleware"
 
@@ -14,7 +15,6 @@ import { login } from "./actions/userActions"
 
 import { fatchTodos , createTodos, deleteTodos } from "./tests/testRedusers"
 
-import logger from "redux-logger"
 
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
@@ -22,7 +22,8 @@ import 'semantic-ui-css/semantic.min.css';
 const reducers = combineReducers({
   user: userReducer,
   todosState: todoReducer
-})
+});
+
 const sagaMiddleware = createSagaMiddleware();
 const middleware = applyMiddleware( sagaMiddleware ,logger );
 const store = createStore(reducers, middleware);
