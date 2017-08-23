8cc7805d4245b6df727f944317f3f424c79145d1
git <djalminya@gmail.com>
2017-08-23 15:19:13 +0300

    decouple networklayer to actions

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,8 +1,12 @@
-import axios from "axios";
-import setAuthororizationToken from "../Auth/setAuthorizationToken";
-import jwt_decode from "jwt-decode";
-
 
 export function login(loginData) {
     return {type: "USER_LOGIN_PENDING", payload: loginData};
+}
+
+export function loginSucceed(userData) {
+  return {type: "USER_LOGIN_FULFILLED", payload: userData };
+}
+
+export function loginFailed(errorData) {
+  return {type: "USER_LOGIN_REJECTED", payload: errorData};
 }
--- src/networkLayer/api-middleware.js
+++ src/networkLayer/api-middleware.js
@@ -2,6 +2,7 @@ import { takeEvery } from "redux-saga"
 import { call, put } from "redux-saga/effects"
 import axios from "axios"
 import setAuthorizationToken from "../Auth/setAuthorizationToken"
+import { loginSucceed, loginFailed } from "../actions/userActions"
 import jwt_decode from "jwt-decode";
 
 export function* loginAsync(action) {
@@ -9,9 +10,10 @@ export function* loginAsync(action) {
       const { data } = yield call(axios.post, "http://todos.moonsite.co.il/api/login", action.payload);
       const token = data.token;
       setAuthorizationToken(token);
-      yield put({type: "USER_LOGIN_FULFILLED", payload: jwt_decode(token)});
+
+      yield put(loginSucceed(jwt_decode(token)));
   } catch (e) {
-      yield put({type: "USER_LOGIN_REJECTED", payload: e});
+      yield put(loginFailed(e));
   }
 }
 
